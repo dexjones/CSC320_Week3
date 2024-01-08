8652b29cdf7e4ad041ca705f5abfc8bd512c6ede
Dexter <dexter.t.jones@outlook.com>
2024-01-07 19:42:24 -0700

    Changes: Added taxRate, user input varification, psuedocode

--- src/Tax.java
+++ src/Tax.java
@@ -1,3 +1,26 @@
+/*
+Written by Dexter Jones
+Date: 1/7/2024
+
+Psuedocode:
+A program that will calculate the weekly average tax withholding for a customer given the following weekly income
+guidelines:
+Income less than $500: tax rate 10%
+Income greater than/equal to $500 and less than $1500: tax rate 15%
+Income greater than/equal to $1500 and less than $2500: tax rate 20%
+income greater than/equal to $2500: tax rate 30%
+
+User input: weekly income
+
+Output: tax rate
+
+Tax rate class: returns calculated tax rate (has guidelines for calculation)
+Withholdiong class: returns the calculated withheld taxes from weekly income
+
+User input validation: Ask for a string, try-catch to make sure input is number
+
+
+ */
 import java.util.*;
 public class Tax {
     public static double taxRate(double income) {
@@ -16,10 +39,16 @@ public class Tax {
         }
         return rate;
     }
+    public static double avgTax(double income, double rate) {
+        double avg;
+        avg = income * rate;
+
+        return avg;
+    }
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
 
-        double income = 0, rate;
+        double income = 0, rate, withholding;
         String s;
         boolean isNumber = false;
         System.out.println("Enter weekly income: $");
@@ -36,7 +65,9 @@ public class Tax {
             }
         }
         rate = taxRate(income);
-        System.out.println(rate);
+        withholding = avgTax(income, rate);
+
+        System.out.println("The tax rate is %" + (rate*100) + " and the weekly average tax withholding is $" + withholding);
 
     }
 }
